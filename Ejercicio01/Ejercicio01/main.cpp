b8e11f2ffb9e884295480cdbc8e3241564422450
Jorge Iriarte <106839094+joririarte@users.noreply.github.com>
2024-03-24 13:19:38 -0300

    Delete Ejercicio01/Ejercicio01 directory

--- Ejercicio01/Ejercicio01/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <ctime>
-int main(int argc, char ** argv){
-    srand(time(NULL));
-    for(int i=1; i<=10;i++){
-        std::cout << "Numero " << i <<": " << rand()%19 + 2 << std::endl;
-    }
-    return 0;
-}
